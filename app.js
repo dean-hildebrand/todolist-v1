154b533e66030044f067108b8447872b2fe6b5cd
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-07-18 13:07:34 -0400

    create post route with a form and logged user input

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
-app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res){
+app.set("view engine", "ejs");
 
-  let today = new Date()
+app.get("/", function(req, res) {
+  let today = new Date();
 
   let options = {
-    weekday: 'long', day: 'numeric', month: 'long'
-  }
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  let day = today.toLocaleDateString('en-US', options)
+  let day = today.toLocaleDateString("en-US", options);
 
-  res.render('list', {
+  res.render("list", {
     kindOfDay: day
-  })
-})
+  });
+});
 
+app.post("/", function(req, res) {
+  let input = req.body.todo;
 
+  console.log(input);
+});
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server is up and running on port 3000");
-})
+});
