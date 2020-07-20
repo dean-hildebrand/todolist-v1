b0b3bf704a95dd2e2916fd6caf5a8e07a6c0f2d2
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-07-20 15:34:09 -0400

    Create our own module and exported it to app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+// created our own module and require it with this const
+const date = require(__dirname + "/date.js")
+
 
 const app = express();
 
@@ -12,15 +15,8 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-  let today = new Date();
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  let day = date()
 
   res.render("list", {
     listTitle: day,
