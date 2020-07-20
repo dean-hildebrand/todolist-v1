647b64f3dd06dad232f54d8e069726beb20785ca
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-07-20 15:06:50 -0400

    use ejs to change value of btn to add to separate lists on separate routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Feed the dog", "Walk the dog"];
+let workItems = []
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -22,18 +23,35 @@ app.get("/", function(req, res) {
   let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    kindOfDay: day,
-    newListItemArray: items
+    listTitle: day,
+    newListItems: items
   });
 });
 
 app.post("/", function(req, res) {
+
   let item = req.body.newItem;
+console.log(req.body.list);
+  if(req.body.list === "Work"){
+      workItems.push(item)
+      res.redirect('/work')
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
-  items.push(item);
-
-  res.redirect("/");
 });
+
+app.get('/work', function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems})
+})
+
+
+app.post('/work', function(req, res){
+  let item = req.body.newItem
+  workItems.push(item)
+  res.redirect('/work')
+})
 
 app.listen(3000, function() {
   console.log("Server is up and running on port 3000");
